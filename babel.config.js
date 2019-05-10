bcb19413e0473afc4217b6000c808d0e4dae8d94
Trevor Lemon <duhlemman@gmail.com>
2019-05-10 16:06:50 -0700

    Brought in a2rp Babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,52 +1,14 @@
-const { parse, relative } = require('path');
 const { version } = require('./package.json');
 const { aliases } = require('./conf.app');
 
 module.exports = (api) => {
   api.cache(true);
   
-  // map alias' so files resolve
-  const _aliases = {};
-  const aliasKeys = [];
-  Object.keys(aliases).forEach((alias) => {
-    _aliases[alias] = aliases[alias];
-    aliasKeys.push(alias);
-  });
-  const aliasRegEx = new RegExp(`^(${ aliasKeys.join('|') })`);
-  const moduleResolver = ['module-resolver', {
-    alias: _aliases,
-    resolvePath(sourcePath, currentFile, opts) {
-      const match = sourcePath.match(aliasRegEx);
-      
-      if( match ){
-        const alias = match[1];
-        const transformed = sourcePath.replace(alias, _aliases[alias]);
-        let relativePath = relative(
-          parse(currentFile).dir,
-          transformed
-        );
-        
-        if(!relativePath.startsWith('.')) relativePath = `./${ relativePath }`;
-        
-        // console.log(
-        //   '---\n',
-        //   `file: ${ currentFile }\n`,
-        //   `requested: ${ transformed }\n`,
-        //   `relative: ${ relativePath }\n`,
-        // );
-        
-        return relativePath;
-      }
-      
-      return sourcePath;
-    },
-  }];
-  
   // Settings used for building npm distributable module
   // https://babeljs.io/docs/en/6.26.3/babel-preset-env
   return {
     plugins: [
-      moduleResolver,
+      ['@noxx/babel-plugin-a2rp', { aliases }],
       ['transform-define', {
         'global.appVersion': `v${ version }`,
       }],

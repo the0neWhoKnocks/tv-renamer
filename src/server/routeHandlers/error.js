fcd8805b4b77ae89fc8c70530eea8b1c380cd040
Trevor Lemon <duhlemman@gmail.com>
2020-06-07 08:54:22 -0700

    Added debug logging for possible timeout scenario

--- src/server/routeHandlers/error.js
+++ src/server/routeHandlers/error.js
@@ -1,10 +1,16 @@
 export default ({ res }, code, msg) => {
   let transformedError = `${ msg }`;
   
-  if(typeof msg === 'object' && msg.stack)
-    transformedError = `${ msg.stack }`;
+  if(typeof msg === 'object' && msg.stack){
+    transformedError = msg.stack;
+    
+    if(transformedError.includes('TIMEDOUT')){
+      // TODO - print out the URL that timed out
+      console.log(Object.keys(res));
+    }
+  }
   
-  console.error('[ ERROR ]', transformedError);
+  console.error('[ERROR]', transformedError);
   
   res.statusCode = code;
   res.end(transformedError);

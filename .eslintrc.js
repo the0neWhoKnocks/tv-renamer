3d1cbd6464ba8c4c79da096c196f95fe227de501
Trevor Lemon <duhlemman@gmail.com>
2020-09-20 12:08:53 -0700

    ops: Upgraded ESLint
    - Wanted to use `ignorePatterns` so I could ignore `release.js` since it's source comes from another repo with different linting rules.
    - Had to update config to account for new rulesets in ESLint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,26 @@
 module.exports = {
+  env: {
+    es6: true,
+  },
   extends: '@noxx',
+  ignorePatterns: [
+    '/bin/release.js',
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
   plugins: [
 		'require-jsdoc-except',
 	],
   rules: {
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+      imports: 'always-multiline',
+      objects: 'always-multiline',
+    }],
     'object-property-newline': 'off',
     'react/no-unused-prop-types': 1,
     'require-jsdoc': 'off',

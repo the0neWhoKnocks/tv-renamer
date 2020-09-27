0f94967514544ed5486f254e384a017a69c76c44
Trevor Lemon <duhlemman@gmail.com>
2020-09-27 12:30:06 -0700

    ops: Update release config to only build and run the App
    It was building/running the App and e2e during the confirmation step.

--- bin/release-config.js
+++ bin/release-config.js
@@ -4,9 +4,9 @@ module.exports = {
   // URL that the App will be available at once started.
   APP__TEST_URL: 'http://localhost:9000',
   // Command to build specific or all Docker containers
-  CMD__DOCKER_BUILD: 'docker-compose build',
+  CMD__DOCKER_BUILD: 'docker-compose build tv-renamer',
   // Command to start the built Container(s), so a Dev can verify before it's deployed
-  CMD__DOCKER_START: 'docker-compose up -d',
+  CMD__DOCKER_START: 'docker-compose up -d tv-renamer',
   // Command to compile any assets that may be needed by Docker, shipped off to S3, etc.
   CMD__COMPILE_ASSETS: 'npm run build:appForDocker',
   // Name of the Docker image: <DOCKER_USER>/<NAME>

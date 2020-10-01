9f1051726ebdfed9fb673ccc29d70b60d0130c96
Trevor Lemon <duhlemman@gmail.com>
2020-10-01 11:21:45 -0700

    fix: camelCase throws when malformed Strings are passed
    The API stated that specific named patterns would be used for episode groups, but it turns out those were just a guide, and there are all sorts of random group names with misc. characters passed. Added extra logic to strip out any non-alpha-numeric characters before trying to change the case of words.

--- src/server/routeHandlers/api/v1/utils/camelCase.js
+++ src/server/routeHandlers/api/v1/utils/camelCase.js
@@ -1,3 +1,8 @@
-const camelCase = (str) => str.toLocaleLowerCase().replace(/\s+(\w)?/gi, (m, l) => l.toUpperCase());
+const camelCase = (str) => str
+  .toLocaleLowerCase()
+  // kill any non alpha-numeric chars (but leave spaces)
+  .replace(/[^a-zA-Z0-9 ]/g, '')
+  // capitalize any words with a leading space (and remove the space)
+  .replace(/\s+(\w)?/gi, (m, l) => l.toUpperCase());
 
 export default camelCase;

2d247d8a49a5eabc1e706bb9e382317d538ae995
Trevor Lemon <duhlemman@gmail.com>
2021-01-12 15:03:54 -0800

    fix: Random genCacheName error
    Put in a stop-gap so that if `undefined` somehow sneaks in there, it won't tip the Server over.

--- src/server/routeHandlers/api/v1/utils/genCacheName.js
+++ src/server/routeHandlers/api/v1/utils/genCacheName.js
@@ -1,13 +1,21 @@
 import sanitizeFilename from 'sanitize-filename';
 
 export default (name) => {
-  const { year } = ((name.match(/\((?<year>\d{4})\)$/) || {}).groups || {});
-  let _name = sanitizeFilename(name)
-    .toLowerCase()
-    .replace(/\s/g, '_')
-    .replace(/[,;"'|()]/g, '');
+  // NOTE - this is just a failsafe, name should always be passed, but I've seen
+  // it randomly be undefined after a Server restart. If I refresh and try
+  // again, things behave as expected. Since I can't debug or reproduce, this
+  // stop-gap will have to suffice for now.
+  let _name = name || '_no_name_';
   
-  if(year) _name = _name.replace(new RegExp(`${ year }$`), `(${ year })`);
+  if(_name){
+    const { year } = ((_name.match(/\((?<year>\d{4})\)$/) || {}).groups || {});
+    _name = sanitizeFilename(_name)
+      .toLowerCase()
+      .replace(/\s/g, '_')
+      .replace(/[,;"'|()]/g, '');
+    
+    if(year) _name = _name.replace(new RegExp(`${ year }$`), `(${ year })`);
+  }
   
   return _name;
 };

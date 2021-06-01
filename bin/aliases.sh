c29e8510f4cc8fff045bbf4d608cf0588128b9aa
Trevor Lemon <duhlemman@gmail.com>
2021-06-01 14:07:56 -0700

    ops: Give the container more time to start before entering it

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -8,5 +8,8 @@ if [[ "${WSL_INTEROP}" != "" ]]; then
   WSL2_ENV_VARS="-e WSL_INTEROP=\"${WSL_INTEROP}\" -e CHOKIDAR_USEPOLLING=1"
 fi
 
-alias starttvrenamer="docker-compose up -d ${CONTAINER} & sleep 2 && docker exec ${WSL2_ENV_VARS} -it ${CONTAINER} zsh && docker-compose down"
+# TODO - create a check script that verifies the container is up instead of using sleep
+# https://stackoverflow.com/a/57536744/5156659
+
+alias starttvrenamer="docker-compose up -d ${CONTAINER} & sleep 4 && docker exec ${WSL2_ENV_VARS} -it ${CONTAINER} zsh && docker-compose down"
 alias entertvrenamer="docker-compose exec ${CONTAINER} zsh"

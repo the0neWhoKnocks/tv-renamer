aadd114b264dbc02689161ac91b27f54c148cd11
Trevor Lemon <duhlemman@gmail.com>
2021-07-15 15:01:03 -0700

    fix: Trailing comma in multi-episode names
    In some cases what `Part <number>` is called out, it's in the format `, Part <number>`.
    Added logic to account for a possible leading comma.

--- src/server/routeHandlers/api/v1/utils/buildEpTitle.js
+++ src/server/routeHandlers/api/v1/utils/buildEpTitle.js
@@ -10,7 +10,7 @@ export default function buildEpTitle(seasons, season, episodes) {
           // Multi-part episodes can end with `(1), Part 1, etc`. Those items need
           // to be removed so a comparison can be made to see if the titles are
           // equal.
-          .replace(/\s(?:\(\d+\)|Part \d+)$/i, '');
+          .replace(/(?: \(\d+\)|,? Part \d+)$/i, '');
         
         // Only add the name if it doesn't already exist.
         if (epNames.indexOf(parsedName) < 0) epNames.push(parsedName);
